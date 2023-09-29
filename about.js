e1f6c4a1fec0d432e3b60f3396c19f3fd8f69e58
Spencer Cox <Spencercoxinbox@gmail.com>
2023-09-29 10:15:52 -0600

    home.html is complete

--- about.js
+++ about.js
@@ -5,10 +5,21 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('form submited');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+
+let ducky = document.querySelector('#duckImg')
+
+
+ducky.addEventListener('mouseover', () => {
+	alert("The ducky has now passed it's coolness off to you!")
+})
+
+
